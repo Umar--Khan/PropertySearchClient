3e4605871c9c84727ffa66294f6526d6a10a57f1
Umar--Khan <umarkhandev@gmail.com>
2019-06-05 15:15:57 +0100

    firstMergeAfterTheme

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from "react";
 
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import LatestProperties from "./components/LatestProperties";
+
 import "./App.css";
+
+// Bootstrap CSS File
+import "./lib/bootstrap/css/bootstrap.min.css";
+
+// Libraries CSS Files
+import "./lib/font-awesome/css/font-awesome.min.css";
+import "./lib/animate/animate.min.css";
+import "./lib/ionicons/css/ionicons.min.css";
+import "./lib/owlcarousel/assets/owl.carousel.min.css";
+
+// Main Stylesheet File
+import "./css/style.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello</h1>
-      </div>
+      <>
+        <NavBar />
+        <Home />
+        <LatestProperties />
+      </>
     );
   }
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+
+class NavBar extends Component {
+  render() {
+    return (
+      <div id="navBarPage">
+        <nav className="navbar navbar-default navbar-trans navbar-expand-lg fixed-top">
+          <div className="container">
+            <button
+              className="navbar-toggler collapsed"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarDefault"
+              aria-controls="navbarDefault"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span />
+              <span />
+              <span />
+            </button>
+            <a className="navbar-brand text-brand" href="index.html">
+              Move<span className="color-b">Right</span>
+            </a>
+            <button
+              type="button"
+              className="btn btn-link nav-search navbar-toggle-box-collapse d-md-none"
+              data-toggle="collapse"
+              data-target="#navbarTogglerDemo01"
+              aria-expanded="false"
+            >
+              <span className="fa fa-search" aria-hidden="true" />
+            </button>
+            <div
+              className="navbar-collapse collapse justify-content-center"
+              id="navbarDefault"
+            >
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <a className="nav-link active" href="index.html">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="about.html">
+                    About
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="property-grid.html">
+                    Property
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="contact.html">
+                    Contact
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <button
+              type="button"
+              className="btn btn-b-n navbar-toggle-box-collapse d-none d-md-block"
+              data-toggle="collapse"
+              data-target="#navbarTogglerDemo01"
+              aria-expanded="false"
+            >
+              <span className="fa fa-search" aria-hidden="true" />
+            </button>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default NavBar;
