e143833fb8c386772133e4d77c6511bd3e03d12e
Umar--Khan <umarkhandev@gmail.com>
2019-06-19 01:05:15 +0100

    bug fix for image slider

--- src/components/rentListingsPage/SinglePropertyRent.js
+++ src/components/rentListingsPage/SinglePropertyRent.js
@@ -27,7 +27,7 @@ class SingleProperty extends Component {
           this.props.singleProperty.redirect_url
         }`
       )
-      .then(res =>
+      .then(res => {
         axios
           .get(
             `https://cors-anywhere.herokuapp.com/${
@@ -35,12 +35,20 @@ class SingleProperty extends Component {
             }`
           )
           .then(pls => {
-            this.setState({
-              urls: JSON.parse(this.sanitazeData(pls.data).join(""))
-            });
+            if (pls.config.url.includes("zoopla")) {
+              this.setState({
+                urls: JSON.parse(this.sanitazeData(pls.data).join(""))
+              });
+            } else {
+              this.setState({
+                urls: this.backupUrls()
+              });
+            }
           })
-          .catch(err => console.log(err))
-      );
+          .catch(err =>
+            console.log(err, this.props.singleProperty.redirect_url)
+          );
+      });
   }
 
   numberWithCommas = x => {
@@ -102,6 +110,58 @@ class SingleProperty extends Component {
       M.Tabs.init(el);
     }
   }
+
+  backupUrls = () => {
+    return {
+      pictures: [
+        {
+          path:
+            "https://lid.zoocdn.com/645/430/cb907b09f20d517020ac6ec47df2ab9a62d57987.jpg",
+          caption: ""
+        },
+        {
+          path:
+            "https://lid.zoocdn.com/645/430/2669ddeea3833f3a06550682b65c5267b8f6b07c.jpg",
+          caption: ""
+        },
+        {
+          path:
+            "https://lid.zoocdn.com/645/430/b4cbef78217aae4817de31dc45c7592d1fa4a610.jpg",
+          caption: ""
+        },
+        {
+          path:
+            "https://lid.zoocdn.com/645/430/aa368e61116c4b52865f1a0d1e95cbd7bebf19ea.jpg",
+          caption: ""
+        },
+        {
+          path:
+            "https://lid.zoocdn.com/645/430/c61955b03f7c18ecd2bb0ffabfa51c1c3fa72d25.jpg",
+          caption: ""
+        },
+        {
+          path:
+            "https://lid.zoocdn.com/645/430/6ae543f544f13b9c377ad8a57d8489a747f03ef5.jpg",
+          caption: ""
+        },
+        {
+          path:
+            "https://lid.zoocdn.com/645/430/c7cc7c64f0a48d685c0dd2dd112b5bc3c0a35afe.jpg",
+          caption: ""
+        },
+        {
+          path:
+            "https://lid.zoocdn.com/645/430/41bbe109ea46b2cce9cc254827599c32425cb342.jpg",
+          caption: ""
+        },
+        {
+          path:
+            "https://lid.zoocdn.com/645/430/0ff7a6191f27b6938a070c9f0f61392b8cb72635.jpg",
+          caption: ""
+        }
+      ]
+    };
+  };
 
   render() {
     const { singleProperty } = this.props;
